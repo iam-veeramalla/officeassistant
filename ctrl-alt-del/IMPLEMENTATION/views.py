37e5fd7e4015704149f12bffa50b9e0296f14f0e
Abhishek VEERAMALLA <a.veeramalla@f5.com>
2020-07-25 13:18:24 +0530

    intial commit for Office Assistant

--- ctrl-alt-del/IMPLEMENTATION/views.py
+++ ctrl-alt-del/IMPLEMENTATION/views.py
@@ -5,7 +5,11 @@ import datetime
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from IMPLEMENTATION.models import Post
+from IMPLEMENTATION.models import Selection
 from django.views.generic import TemplateView
+from IMPLEMENTATION.forms import SignUpForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 #from django.contrib.auth.forms import UserCreationForm
